0c76d328ed3df64f5ec66896cc23efca7e3ff6af
John Chung <101917927+JohnChung2002@users.noreply.github.com>
2022-11-21 14:29:27 +0800

    Update progress

--- app/src/main/java/com/example/littlegardener/DashboardActivity.kt
+++ app/src/main/java/com/example/littlegardener/DashboardActivity.kt
@@ -3,8 +3,11 @@ package com.example.littlegardener
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.constraintlayout.widget.Group
 import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
